98e596f91e78845731361218426a419b7612aabb
Bishowjit-Ray <82141518+Bishowjit-Ray@users.noreply.github.com>
2022-09-22 09:21:53 +0600

    Add files via upload

---
+++ Showing alert messages/Showing alert messages/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Showing alert messages
+//
+//  Created by Bishowjit Ray on 22/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding()
+           
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Showing alert messages/Showing alert messages/Showing_alert_messagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Showing_alert_messagesApp.swift
+//  Showing alert messages
+//
+//  Created by Bishowjit Ray on 22/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Showing_alert_messagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
